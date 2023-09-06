f2866bb966a4ae3af9d824f379e46dc4070ed0cf
Lotifa Akan Anannya <129364460+Lotifa-Akan-Anannya@users.noreply.github.com>
2023-09-06 22:36:59 +0600

    Create problem11.js

---
+++ problem11.js
@@ -0,0 +1,21 @@
+class Person{
+  constructor(name,age,country){
+    this.name=name;
+    this.age=age;
+    this.country=country;
+  }  
+  
+  displayDetails() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Age: ${this.age}`);
+    console.log(`Country:${this.country}`);
+  }
+}
+const person1 = new Person("Anu",23,"Bangladesh");
+const person2 = new Person("Mim",24,"Japan");
+
+console.log("Person 1 details:");
+person1.displayDetails();
+console.log("/nPerson 2 details:");
+person2.displayDetails();
+
